829be89d3208b1d59e27d9af1b471cdca40aa0ff
Almaz Atangulov <j1hnwall@yandex.ru>
2022-06-05 23:03:05 +0500

    fix: исправлен код JS

--- script/index.js
+++ script/index.js
@@ -1,39 +1,34 @@
-const buttonEdit = document.querySelector('.button__edit');
+//объявил все переменные
 const popup = document.querySelector('.popup');
+const profileButtonEdit = document.querySelector('.profile__button-edit');
 const popupButtonClose = document.querySelector('.popup__button-close');
 const formElement = document.querySelector('.popup__form');
-const nameInput = document.querySelector('.popup__input_name');
-const jobInput = document.querySelector('.popup__input_job');
-const saveButton = document.querySelector('.popup__button-save');
+const nameInput = document.querySelector('.popup__input_type_name');
+const professionInput = document.querySelector('.popup__input_type_profession');
 const profileName = document.querySelector('.profile__name');
 const profileProfesseion = document.querySelector('.profile__profession');
 
+//Описал функции
 function popupOpen () {
   popup.classList.add('popup_opened');
-}
+};
 
 function popupClose () {
   popup.classList.remove('popup_opened');
-}
-
-buttonEdit.addEventListener('click', function () {
-  popupOpen ()
-});
-
-popupButtonClose.addEventListener('click', function () {
-  popupClose ()
-});
-
-function formSubmitHandler (evt) {
-  evt.preventDefault();
 };
 
-saveButton.addEventListener('click', function () {
+function formSubmitHandler (e) {
+  e.preventDefault();
   profileName.textContent = nameInput.value;
-  profileProfesseion.textContent = jobInput.value;
-  popupClose ()
-});
+  profileProfesseion.textContent = professionInput.value;
+  /**  Получил значения полей по их свойству value и выбрал элементы,
+  куда нужно вставить эти значения с помощью свойства textContent */
+  popupClose ();
+};
 
+//Установил события
+profileButtonEdit.addEventListener('click', popupOpen);
+popupButtonClose.addEventListener('click', popupClose);
 formElement.addEventListener('submit', formSubmitHandler);
 
 
